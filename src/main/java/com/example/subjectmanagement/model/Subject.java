1bc38c10a5c2120c53f3775fc630ad18c1893cda
gdcupe <gerardo20guitarra@gmail.com>
2024-09-28 22:27:17 -0500

    Examen lenguaje de programacio II

---
+++ src/main/java/com/example/subjectmanagement/model/Subject.java
@@ -0,0 +1,49 @@
+package com.example.subjectmanagement.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "SUBJECT")
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idsubject")
+    private int idSubject;
+
+    @Column(name = "subject", length = 45, nullable = false)
+    private String subject;
+
+    @Column(name = "credits", length = 45, nullable = false)
+    private String credits;
+
+    // Getters y Setters
+    public int getIdSubject() {
+        return idSubject;
+    }
+
+    public void setIdSubject(int idSubject) {
+        this.idSubject = idSubject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getCredits() {
+        return credits;
+    }
+
+    public void setCredits(String credits) {
+        this.credits = credits;
+    }
+}
